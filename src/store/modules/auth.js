724fcd1e036ab3b615d3e7b86934655120d3c28f
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 09:40:12 +1100

    Section 5 Lesson 92 - Auth Module State and Getters

---
+++ src/store/modules/auth.js
@@ -0,0 +1,18 @@
+const state = {
+    token: null
+};
+
+const getters = {
+    // the updated state object, not the state object in this file!
+    isLoggedIn: (state) => {
+
+    }
+};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
