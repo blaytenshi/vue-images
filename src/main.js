6ff8df52f3f3d8c34175a91351d2228d281b4d54
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 09:14:08 +1100

    Section 5 Lesson 89 - Connecting Vuex to Vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
 
 new Vue({
   render: h => h(App),
+  store, // the same as store: store, kinda like wrapping redux store to react application
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex); // this connects vue with vuex like a middleware
+
+export default new Vuex.Store({
+    modules: {
+        
+    }
+})
