7774f5af72e782a1237647f0b1e40728460bbaad
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 11:19:49 +1100

    Section 5 Lesson 100 - Extracting the Access Token

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,13 +3,21 @@
         <a href="/" class="active item">
             Image Storage
         </a>
+        <div class="right menu">
+            <a href="#" class="ui item" @click="login">Login</a>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     name: 'AppHeader',
-
+    methods: { // alternatively you can also just do methods: mapActions(['login'])
+        ...mapActions(['login']), // this is a list of function names you wanna map into the methods property
+        // i prefer this syntax because it allows you to add other methods on should you need it.
+    }
 }
 </script>
 
