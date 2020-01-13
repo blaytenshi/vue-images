905197d092d56710ecd39f37472ffcdd8fc6b04e
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 14:17:28 +1100

    Section 6 Lesson 115 - Additional Route Config

--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import store from './store';
 import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList';
+import UploadForm from './components/UploadForm';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history', // tells VueRouter to use BrowserRouter instead of HashRouter
   routes: [
-    { path: '/oauth2/callback', component: AuthHandler }
+    { path: '/oauth2/callback', component: AuthHandler },
+    { path: '/', component: ImageList },
+    { path: '/upload', component: UploadForm },
   ]
-})
+});
 
 new Vue({
   render: h => h(App),
