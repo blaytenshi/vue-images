78c334ca8745d7051d457842a5641f28fa946b89
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 11:26:58 +1100

    Section 6 Lesson 101 - Wiring up Vue Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import store from './store';
+import AuthHandler from './components/AuthHandler';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: '/oauth2/callback', component: AuthHandler }
+  ]
+})
 
 new Vue({
   render: h => h(App),
   store, // the same as store: store, kinda like wrapping redux store to react application
+  router, // importing the router~
 }).$mount('#app')
