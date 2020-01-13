d45a4fa54081856e3fbc86ba2d532875d3469271
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 14:22:33 +1100

    Section 6 Lesson 116 - Navigation with Router-Link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">
+        <router-link to="/" class="active item">
             Image Storage
-        </a>
+        </router-link>
         <div class="right menu">
             <div class="horizontal" v-if="isLoggedIn">
                 <router-link to="/" class="item">Galleries</router-link>
