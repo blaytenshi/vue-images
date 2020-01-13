cb2b8fdd9f356ee3cf5f6b16cf8173d0d6d7e297
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 13:39:09 +1100

    Section 6 Lesson 107 - Data in Components with MapGetters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,6 +3,7 @@
         <a href="/" class="active item">
             Image Storage
         </a>
+        {{ isLoggedIn }}
         <div class="right menu">
             <a href="#" class="ui item" @click="login">Login</a>
         </div>
@@ -10,13 +11,20 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: 'AppHeader',
+    // methods accept arguments, used to respond to events from the DOM
     methods: { // alternatively you can also just do methods: mapActions(['login'])
         ...mapActions(['login']), // this is a list of function names you wanna map into the methods property
         // i prefer this syntax because it allows you to add other methods on should you need it.
+    },
+    // computed does NOT accept arguments, used to recompute values as needed from other sources (eg data)
+    computed: {
+        // same as mapActions but this maps the Getters instead.
+        // we put this in computed prop because 
+        ...mapGetters(['isLoggedIn'])
     }
 }
 </script>
