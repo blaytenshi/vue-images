a022b355479f2087c1926194e244b617d7c38786
Jimmy Tang <jimmy.tang@growsuper.com>
2020-01-13 11:54:41 +1100

    Section 6 Lesson 105 - Calling Actions

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -6,10 +6,16 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     name: 'AuthHandler',
+    methods: {
+        ...mapActions(['finalizeLogin'])
+    },
     created() { // lifecycle method! just like react! WOO
-
+        // just like accessing a data or method prop, just use 'this'
+        this.finalizeLogin(window.location.hash) // the hash prop gives you everything after the # in the URL so basically all the params
     }
 }
 </script>
